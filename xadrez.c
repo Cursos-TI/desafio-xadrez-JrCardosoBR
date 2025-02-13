6a8dc8fab4c8e3c92b1c6eff0aa114105c293ace
JrCardosoBR <87071738+JrCardosoBR@users.noreply.github.com>
2025-02-13 17:40:52 -0300

    Correção Peça Cavalo

--- xadrez.c
+++ xadrez.c
@@ -27,7 +27,7 @@ int main() {
     
     // Movimentação do Cavalo
     printf("\nMovimentação do Cavalo:\n"); 
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < CAVALO; i++) {
         printf("Baixo\n");
     }
     int j = 0;
