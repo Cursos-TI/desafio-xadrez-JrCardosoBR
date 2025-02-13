e2a2af18b167265f036beee68ebf5957f9e5dd15
JrCardosoBR <87071738+JrCardosoBR@users.noreply.github.com>
2025-02-13 17:39:36 -0300

    Desafio Aventureiro Concluido
    Adicionado a peça cavalo, utilizando loop aninhadas para movimentação da peça cavalo

--- xadrez.c
+++ xadrez.c
@@ -3,8 +3,10 @@
 #define BISPO 5
 #define TORRE 5
 #define RAINHA 8
+#define CAVALO 2
 
 int main() {
+
     // Movimentação do Bispo
     printf("Movimentação do Bispo:\n");
     for (int i = 0; i < BISPO; i++) {
@@ -22,6 +24,17 @@ int main() {
     for (int i = 0; i < RAINHA; i++) {
         printf("Esquerda\n");
     }
-
+    
+    // Movimentação do Cavalo
+    printf("\nMovimentação do Cavalo:\n"); 
+    for (int i = 0; i < 2; i++) {
+        printf("Baixo\n");
+    }
+    int j = 0;
+    while (j < 1) {
+        printf("Esquerda\n");
+        j++;
+    }
+     
     return 0;
 }
